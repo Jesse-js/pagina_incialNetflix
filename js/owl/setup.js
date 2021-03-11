5c8478b6123465312374647442ee8cc2566051ce
Jesse-js <jesse-jjs@hotmail.com>
2021-03-11 11:37:27 -0300

    ProjetoStarFlix

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function site(){
+    window.open("https://www.youtube.com/watch?v=ZLW2jkd6E7g");
+}
